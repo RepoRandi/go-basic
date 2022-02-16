852ad11e492c9f6a08aa82bceb3f5d1d11151947
Randi <randimaulanaakbar@gmail.com>
2022-02-16 18:35:43 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Randi"
+	fmt.Println(name)
+
+	var country = "US"
+
+	fmt.Println(country)
+
+	age := 17
+	fmt.Println(age)
+
+	var (
+		firstName = "Sugi"
+		lastName  = "Manggala"
+	)
+
+	fmt.Println(firstName, lastName)
+}
