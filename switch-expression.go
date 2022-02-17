05f3dd23bdaf966fbe828ea1dfa731814ed7a019
Randi <randimaulanaakbar@gmail.com>
2022-02-17 21:35:34 +0700

    switch expression

--- switch-expression.go
+++ switch-expression.go
@@ -1,12 +1,34 @@
 package main
 
-func main1() {
-	switch name := "Lad"; name {
+import "fmt"
+
+func main16() {
+	name := "Randi"
+
+	switch name {
 	case "Randi":
 		println("Hello", name)
 	case "Lady":
 		println("Hello", name)
 	default:
 		println("Hello stranger")
+	}
+
+	switch len(name) >= 5 {
+	case true:
+		fmt.Println("Name is longer than 5 characters")
+	case false:
+		fmt.Println("Name is less than 5 characters")
+	}
+
+	length := len(name)
+
+	switch {
+	case length > 5:
+		fmt.Println("Name is longer than 5 characters")
+	case length < 5:
+		fmt.Println("Name is less than 5 characters")
+	case length == 5:
+		fmt.Println("Name is 5 characters")
 	}
 }
