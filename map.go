701c0bc6caa4009153efba41a045a8bfb56a0ec8
Randi <randimaulanaakbar@gmail.com>
2022-02-17 16:29:39 +0700

    tipe data map

---
+++ map.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main13() {
+	person := map[string]string{
+		"name": "John",
+		"job":  "Software Engineer",
+	}
+
+	person["email"] = "randi@gmail.com"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["job"])
+	fmt.Println(person["email"])
+
+	var book map[string]string = make(map[string]string)
+
+	book["name"] = "The Go Programming Language"
+	book["author"] = "Alan A. A. Donovan"
+	book["publisher"] = "Addison-Wesley Professional"
+
+	fmt.Println(book)
+
+	delete(book, "publisher")
+
+	fmt.Println(book)
+}
