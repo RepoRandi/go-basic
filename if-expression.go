28320f24bb4f26110740348c193ce967c1d6efeb
Randi <randimaulanaakbar@gmail.com>
2022-02-17 16:48:19 +0700

    if expression

---
+++ if-expression.go
@@ -0,0 +1,19 @@
+package main
+
+func main13() {
+	name := "Lady"
+
+	if name == "Randi" {
+		println("Hello", name)
+	} else if name == "Lady" {
+		println("Hello", name)
+	} else {
+		println("Hello stranger")
+	}
+
+	if len(name) > 5 {
+		println(name, "is a long name")
+	} else {
+		println(name, "is a short name")
+	}
+}
