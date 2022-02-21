1d185e86c3537bcb79caeacd2bb923a7a0515bc2
Randi <randimaulanaakbar@gmail.com>
2022-02-21 13:55:36 +0700

    function as parameter

---
+++ function-as-parameter.go
@@ -0,0 +1,30 @@
+package main
+
+import "strings"
+
+type Filter func(string) string
+
+func main() {
+	sayHelloWithFilter("Anjing", spamFilter)
+	sayHelloWithFilter("Jancok", spamFilter)
+	sayHelloWithFilter("Bambang", spamFilter)
+	sayHelloWithFilter("big e", uppercaseFilter)
+}
+
+func sayHelloWithFilter(name string, filter Filter) {
+	println("Hello", filter(name))
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "A....g"
+	} else if name == "Jancok" {
+		return "J....k"
+	} else {
+		return name
+	}
+}
+
+func uppercaseFilter(name string) string {
+	return strings.ToUpper(name)
+}
