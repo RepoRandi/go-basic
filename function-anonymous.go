7d378c0be136503abaad116ce3080754805a2b97
Randi <randimaulanaakbar@gmail.com>
2022-02-21 14:48:59 +0700

    function anonymous

---
+++ function-anonymous.go
@@ -0,0 +1,25 @@
+package main
+
+type BlackList func(string) bool
+
+func main() {
+	blackList := func(name string) bool {
+		return name == "Anjing" || name == "Jancok"
+	}
+
+	registerUser("Anjing", blackList)
+	registerUser("Jancok", blackList)
+	registerUser("Bambang", blackList)
+}
+
+func registerUser(name string, blackList BlackList) {
+	if blackList(name) {
+		println("User", name, "is blacklisted")
+	} else {
+		println("User", name, "is not blacklisted")
+	}
+}
+
+// func nameBlackList(name string) bool {
+// 	return name == "Anjing" || name == "Jancok"
+// }
