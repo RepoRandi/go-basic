9d2e0d1cf2b8534ab7555285339192c766322922
Randi <randimaulanaakbar@gmail.com>
2022-03-03 22:27:18 +0700

    package and import

---
+++ helper/sayhello.go
@@ -0,0 +1,7 @@
+package helper
+
+import "fmt"
+
+func SayHello(name string) {
+	fmt.Println("SayHello: ", name)
+}
---
+++ package.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"go-basic/helper"
+)
+
+func main() {
+	name := "John"
+	helper.SayHello(name)
+}
