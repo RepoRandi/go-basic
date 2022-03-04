12a0ff17fa4f94112dccd29dcf1e393d0c63faa0
Randi <randimaulanaakbar@gmail.com>
2022-03-04 11:01:01 +0700

    access modifier

---
+++ access-modifier.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"go-basic/helper"
+)
+
+func main46() {
+	app := helper.Application
+
+	fmt.Println("Hello,", app)
+}
--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,13 @@ package helper
 
 import "fmt"
 
+var version = "1.0.0"
+var Application = "Go"
+
 func SayHello(name string) {
 	fmt.Println("SayHello: ", name)
+}
+
+func sayGoodbye(name string) {
+	fmt.Println("sayGoodbye: ", name)
 }
